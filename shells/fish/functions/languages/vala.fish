c1dc772c73d70ef5c2eece3d74a7dc33fbe81512
Jedsek <jedsek@qq.com>
2022-06-06 03:37:12 +0000

    update shells/fish/functions/languages/vala.fish.

--- shells/fish/functions/languages/vala.fish
+++ shells/fish/functions/languages/vala.fish
@@ -2,6 +2,8 @@ function valac -w "valac"
     if string match -rq -- "-C|--ccode" $argv
         command valac $argv
     else
+        ## -X 表示之后的参数是传递给c语言编译器的参数
+        ## -w 表示c编译器会无视所有警告
         command valac -X -w $argv    
     end
 end
