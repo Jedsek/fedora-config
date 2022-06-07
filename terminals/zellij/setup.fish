8c4cae2221e092b147b9c12be8341c861c30bb45
Jedsek <jedsek@qq.com>
2022-06-07 05:38:44 +0000

    update terminals/zellij/setup.fish.

--- terminals/zellij/setup.fish
+++ terminals/zellij/setup.fish
@@ -4,6 +4,3 @@ sudo dnf install -y zellij
 mkdir $nud/.config/zellij
 rm $terminals_dir/zellij/*.fish
 mv $terminals_dir/zellij $nud/.config/
-
-sudo bash -c "echo 'zellij options --no-pane-frames --copy-command wl-copy' > /usr/bin/zj"
-sudo chmod a+x /usr/bin/zj
