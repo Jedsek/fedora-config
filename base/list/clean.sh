c77b995679f66e3ba586ef6663c1acc8bff65217
Jedsek <jedsek@qq.com>
2022-06-07 05:19:17 +0000

    update base/list/clean.sh.

--- base/list/clean.sh
+++ base/list/clean.sh
@@ -3,5 +3,8 @@
 sudo dnf remove -y abrt gnome-abrt gnome-software gnome-weather gnome-contacts gnome-maps gnome-calendar
 sudo dnf remove -y gnome-clocks gnome-disk-utility baobab cheese totem samba-client rhythmbox
 sudo dnf remove -y gnome-tour
+
+sudo dnf remove -y firefox
+
 sudo rm /usr/bin/gnome-help
 sudo dnf clean all
