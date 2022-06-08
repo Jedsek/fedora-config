16294e4df74ab0f371ff7a814585f6c62437f005
Jedsek <jedsek@qq.com>
2022-06-08 04:18:03 +0000

    update code/languages/rust.fish.

--- code/languages/rust.fish
+++ code/languages/rust.fish
@@ -1,9 +1,9 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 echo '[source.crates-io]
-replace-with = \'ustc\'
+replace-with = \'tuna\'
 
-[source.ustc]
-registry = "git://mirrors.ustc.edu.cn/crates.io-index"' > $nud/.cargo/config
+[source.tuna]
+registry = "https://mirrors.tuna.tsinghua.edu.cn/git/crates.io-index.git"' > $nud/.cargo/config
 
 
 sudo dnf copr enable -y robot/rust-analyzer
