82eaecd72e28f43b1d618454ec087dd156bba5e4
Jedsek <jedsek@qq.com>
2022-07-06 05:52:48 +0000

    update gnome/beauty/setup.fish.

--- gnome/beauty/setup.fish
+++ gnome/beauty/setup.fish
@@ -2,6 +2,8 @@ for part in $gnome_dir/beauty/*/setup.fish
     fish -N $part
 end
 
+gsettings set org.gnome.desktop.interface show-battery-percentage true
+
 # xhost +SI:localuser:gdm
 # gsettings set org.gnome.desktop.interface text-scaling-factor 1.2
 # sudo -u gdm gsettings set org.gnome.desktop.interface text-scaling-factor 1.2
