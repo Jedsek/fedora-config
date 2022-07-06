76b6affcb315e9c8a14a4d9233ee455c357fbfea
Jedsek <jedsek@qq.com>
2022-07-06 11:28:26 +0000

    update gnome/setup.fish.

--- gnome/setup.fish
+++ gnome/setup.fish
@@ -1,9 +1,7 @@
 cd (status dirname)
 set -x gnome_dir       (pwd)
-set -x nu      (ls /home/ | head -n 1)
-set -x nud     /home/$nu
 
-sudo chmod -R 777 $nud/.local/
+sudo chmod -R 777 ~/.local/
 
 for part in $gnome_dir/*/setup.fish
     fish -N $part
