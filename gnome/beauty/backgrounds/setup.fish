b45c54b838a07cbe22d0cbc8126f4c31ed1ad47a
Jedsek <jedsek@qq.com>
2022-07-11 07:11:21 +0000

    update gnome/beauty/backgrounds/setup.fish.

--- gnome/beauty/backgrounds/setup.fish
+++ gnome/beauty/backgrounds/setup.fish
@@ -1,4 +1,4 @@
 sudo mkdir /usr/share/backgrounds/user/
 sudo cp -r $gnome_dir/beauty/backgrounds/list/* /usr/share/backgrounds/user/
-gsettings set org.gnome.desktop.background picture-uri 'file:///usr/share/backgrounds/user/F35-01-day.png'
-gsettings set org.gnome.desktop.background picture-uri-dark  'file:///usr/share/backgrounds/user/F35-01-day.png'
+# gsettings set org.gnome.desktop.background picture-uri 'file:///usr/share/backgrounds/user/F35-01-day.png'
+# gsettings set org.gnome.desktop.background picture-uri-dark  'file:///usr/share/backgrounds/user/F35-01-day.png'
