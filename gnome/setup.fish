82d9e1e224cfb28fb6aea60a9a18ad1dbfdf0656
Jedsek <jedsek@qq.com>
2022-07-11 07:10:08 +0000

    update gnome/setup.fish.

--- gnome/setup.fish
+++ gnome/setup.fish
@@ -4,4 +4,4 @@ set -x gnome_dir       (pwd)
 sudo chmod -R 777 ~/.local/
 
 fish -N $gnome_dir/extensions/setup.fish
-cat $gnome_dir/dconf-settings | dconf load /
+cat $gnome_dir/dconf.settings | dconf load /
