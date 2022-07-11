31592aa83a1fd38c03a28971d0404995bee480d8
Jedsek <jedsek@qq.com>
2022-07-11 07:11:00 +0000

    update gnome/beauty/setup.fish.

--- gnome/beauty/setup.fish
+++ gnome/beauty/setup.fish
@@ -2,8 +2,8 @@ for part in $gnome_dir/beauty/*/setup.fish
     fish -N $part
 end
 
-gsettings set org.gnome.desktop.interface show-battery-percentage true
-gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
+# gsettings set org.gnome.desktop.interface show-battery-percentage true
+# gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
 
 # xhost +SI:localuser:gdm
 # gsettings set org.gnome.desktop.interface text-scaling-factor 1.2
