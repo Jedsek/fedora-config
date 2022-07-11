a84c3fcda39b261e4c3a3f4db8624fe070522e7d
Jedsek <jedsek@qq.com>
2022-07-11 12:38:49 +0000

    update code/languages/haskell.fish.

--- code/languages/haskell.fish
+++ code/languages/haskell.fish
@@ -1,6 +1,8 @@
 sudo dnf install -y haskell-platform
 
 echo ':set prompt "ghci> "' > ~/.ghci
+chmod go-w /home/jedsek/.ghci
+
 cabal user-config init
 sed -i 's/repository hackage.haskell.org/repository mirrors.ustc.edu.cn/g' ~/.cabal/config
 sed -i 's/http:\/\/hackage.haskell.org\//http:\/\/mirrors.ustc.edu.cn\/hackage\//g' ~/.cabal/config
