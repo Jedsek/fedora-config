3eea3762ef59f7be64a5434e9604ce7428bf7cd6
Jedsek <jedsek@qq.com>
2022-07-11 07:17:55 +0000

    删除文件 gnome/favorite-apps

--- gnome/favorite-apps/setup.fish
+++
@@ -1,5 +0,0 @@
-gsettings set org.gnome.shell favorite-apps "[]"
-
-gsettings set org.gnome.desktop.default-applications.terminal exec 'alacritty'
-sudo rm /usr/bin/gnome-terminal
-sudo ln -s /usr/bin/alacritty /usr/bin/gnome-terminal
