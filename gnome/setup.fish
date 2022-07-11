0dcec27b4f2d7d2691a89a054ea88038fcff5fb7
Jedsek <jedsek@qq.com>
2022-07-11 07:37:07 +0000

    update gnome/setup.fish.

--- gnome/setup.fish
+++ gnome/setup.fish
@@ -4,6 +4,7 @@ set -x gnome_dir       (pwd)
 sudo chmod -R 777 ~/.local/
 
 fish -N $gnome_dir/extensions/setup.fish
+fish -N $gnome_dir/beauty/setup.fish
 cat $gnome_dir/dconf.settings | dconf load /
 xhost +SI:localuser:gdm
 sudo -u gdm gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
