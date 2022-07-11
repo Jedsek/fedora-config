c2f998367bed8620319d7424749fd85ebf4689c3
Jedsek <jedsek@qq.com>
2022-07-11 07:15:53 +0000

    删除文件 gnome/touchpad

--- gnome/touchpad/setup.fish
+++
@@ -1,8 +0,0 @@
-gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
-gsettings set org.gnome.desktop.peripherals.touchpad speed 0.57
-gsettings set org.gnome.desktop.peripherals.touchpad disable-while-typing false
-
-xhost +SI:localuser:gdm
-sudo -u gdm gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
-sudo -u gdm gsettings set org.gnome.desktop.peripherals.touchpad speed 0.57
-sudo -u gdm gsettings set org.gnome.desktop.peripherals.touchpad disable-while-typing false
