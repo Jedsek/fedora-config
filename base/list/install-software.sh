cf858fd0bdbcf19297bfef41ddeda0f0442a679e
Jedsek <jedsek@qq.com>
2022-07-22 11:19:27 +0000

    update base/list/install-software.sh.

--- base/list/install-software.sh
+++ base/list/install-software.sh
@@ -3,8 +3,9 @@
 sudo dnf install -y util-linux-user
 sudo dnf install -y feh axel neofetch jq bsdtar
 sudo dnf install -y zoxide procs exa bat ripgrep fd-find sd
-sudo dnf install -y wl-clipboard trash-cli
+sudo dnf install -y wl-clipboard
 sudo dnf install -y gtk4-devel
+sudo dnf install -y nautilus
 
 # sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 # sudo dnf config-manager --add-repo https://packages.microsoft.com/yumrepos/edge
